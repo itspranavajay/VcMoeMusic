5df264fcf4a234ae20b1c2db70f69bc0ea925c68
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-06-25 20:05:25 +0530

    Add files via upload

---
+++ MoeMusicBot/services/callsmusic/__init__.py
@@ -0,0 +1,2 @@
+from MoeMusicBot.services.callsmusic import queues
+from MoeMusicBot.services.callsmusic.callsmusic import pytgcalls, run
---
+++ MoeMusicBot/services/downloaders/__init__.py
@@ -0,0 +1,1 @@
+from MoeMusicBot.services.downloaders.youtube import download
